ed833d4037a65e1e34326af1355b47909d85f646
RyanBotzz <109885043+RyanBotzz@users.noreply.github.com>
2022-08-06 11:23:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ global.DeveloperMode = 'false' //true Or false
 global.linkGC = ['https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM', 'https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI']
 global.Owner = {
   // use the phone number with the country code, for example Indonesia '62' America '1' etc
-  '62857139649630': { // put your number here
+  '6285779150501': { // put your number here
     name: 'Owner Metro Bot/Creator games-wabot',
     isDev: false, // if true this number will send if bot have bug and developer mode true
     isReport: false, // if true this number will send if user use command /report or /join
@@ -55,7 +55,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
+global.packname = 'wa.me/6285779150501'
 global.author = 'Metro Bot Is The Best'
 
 global.multiplier = 39 // The higher, The harder levelup
